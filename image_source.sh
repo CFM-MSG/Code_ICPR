334d6dd50b09fdf72d871e3214799debba5d5b49
interxuxing <interxuxing.hust@gmail.com>
2023-03-05 22:25:59 +0800

    fix file conflict
    fix file conflict

--- image_source.sh
+++
@@ -1,6 +0,0 @@
-# VISDA-C
-python image_source.py --trte val --output None --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 15 --s 0 --source_balance 0 --use_prototype 1
-python image_source.py --trte val --output None --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 15 --s 0 --source_balance 0 --use_prototype 0
-python image_source.py --trte val --output None --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 15 --s 0 --source_balance 1 --use_prototype 1
-python image_source.py --trte val --output None --da uda --gpu_id 0 --dset VISDA-C --net resnet101 --lr 1e-3 --max_epoch 15 --s 0 --source_balance 1 --use_prototype 0
-
